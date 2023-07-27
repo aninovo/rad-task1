c5b8fca98f291ae3e8e89e4739181747a3db5308
aninovo <aninovo22@gmail.com>
2023-07-27 14:10:41 +0300

    Shifted to Express.js static server
    All the files moved to ./public

---
+++ public/data.js
@@ -0,0 +1,7 @@
+import { Note } from "./logic.js";
+// instead of an actual data storage
+
+export let data = [
+    new Note('a', 'Idea', 'b 11/12/1023   11/22/1023, 2018.5.2', new Date("1995-12-17T03:24:00")),
+    new Note('a', 'Random Thought', 'b', new Date("1995-12-17T03:24:00")),
+];
