ceda8ceb66066033c0539adb8fed0c0b9782cd4b
aninovo <aninovo22@gmail.com>
2023-07-27 15:47:08 +0300

    Update index.js
    Added createNoteElement(note) that creates a table row representing values of a Note object

--- public/index.js
+++ public/index.js
@@ -1,2 +1,25 @@
 import { data } from "./data.js";
 import { Note } from "./logic.js";
+
+function createNoteElement(note) {
+
+    let fields = {
+        Name: note.name,
+        Created: note.creationTime.toLocaleDateString(),
+        Category: note.category,
+        Content: note.description,
+        Dates: note.dates.map(date => date.toLocaleDateString()).join(', '),
+    };
+
+    let tableRow = document.createElement('tr');
+    for (const field in fields) {
+        let tableTD = document.createElement('td');
+        let text = document.createTextNode(fields[field]);
+        tableTD.appendChild(text);
+        tableRow.appendChild(tableTD);
+    }
+    // todo the buttons
+    return tableRow;
+}
+
+console.log(createNoteElement(data[0]));
